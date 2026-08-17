8943752a193c1e520d231ba4049e2f1a83d20cd5
Clafer03 <claudi125.lol@gmail.com>
2026-08-16 21:20:57 -0500

    chore: configurar proyecto inicial

---
+++ backend/prisma/migrations/20260817020934_create_task/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "Priority" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "priority" "Priority" NOT NULL,
+    "estimated_minutes" INTEGER NOT NULL,
+    "due_date" TIMESTAMP(3),
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
